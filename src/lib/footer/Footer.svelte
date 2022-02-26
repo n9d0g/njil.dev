5a4ec5a95c5e4e4d4a8d643d0799ff0e3d69e5b7
n9d0g <nathanjlardizabal@gmail.com>
2022-02-26 10:45:34 -0500

    commit unpushed files

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+	const weekday = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+	const date = new Date()
+	let day = weekday[date.getDay()]
+</script>
+
+<footer>
+	<p>nathan james lardizabal | have a good {day} :)</p>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
