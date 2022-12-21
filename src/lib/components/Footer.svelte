29eb63bd63f48bedd58cf4a3f8b638e2b4c16741
n9d0g <nathanjlardizabal@gmail.com>
2022-12-21 03:03:56 -0500

    remove fluff

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,23 +1,5 @@
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+<footer class="flex flex-col justify-center items-center p-4">
+	<p>
+		visit <a href="https://kit.svelte.dev" class="font-bold">kit.svelte.dev</a> to learn SvelteKit
+	</p>
 </footer>
-
-<style>
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,29 +4,10 @@
 	import './styles.css';
 </script>
 
-<div class="app">
+<div class="flex min-h-screen flex-col justify-between">
 	<Header />
 	<main>
 		<slot />
 	</main>
 	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
