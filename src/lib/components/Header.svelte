2d8c9f0434d9c13b3b5309bd949d2867da60e41e
n9d0g <nathanjlardizabal@gmail.com>
2022-12-21 03:29:54 -0500

    fix build issues

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,9 +5,9 @@
 <header class="flex justify-center py-4">
 	<nav class="flex justify-center ">
 		<ul class="flex relative justify-center items-center list-none bg-contain p-0 m-0">
-			<HeaderItem href="/" link="home" />
-			<HeaderItem href="/about" link="about" />
-			<HeaderItem href="/experience" link="experience" />
+			<HeaderItem hrefLink="/" link="home" />
+			<HeaderItem hrefLink="/about" link="about" />
+			<HeaderItem hrefLink="/experience" link="experience" />
 		</ul>
 	</nav>
 </header>
--- src/lib/components/HeaderItem.svelte
+++ src/lib/components/HeaderItem.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	export let href: string;
+	export let hrefLink: string;
 	export let link: string;
 </script>
 
 <li class="relative h-full" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 	<a
 		class="flex h-full items-center py-0 px-2 font-bold text-sm uppercase tracking-wide no-underline transition"
-		{href}
+		href={hrefLink}
 	>
 		{link}
 	</a>
