83bf6ffa038d1012b830882f9952fcf658400832
n9d0g <nathanjlardizabal@gmail.com>
2022-12-22 01:58:03 -0500

    refactor: :recycle: format project

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,3 +1,3 @@
-import { writable } from 'svelte/store'
+import { writable } from 'svelte/store';
 
-export const lightTheme = writable(true)
+export const lightTheme = writable(true);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      colors: {
-        'colour-theme-1': 'var(--colour-theme-1)',
-        'colour-theme-2': 'var(--colour-theme-2)',
-        'colour-text': 'var(--colour-text)',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		container: {
+			center: true
+		},
+		extend: {
+			colors: {
+				'colour-theme-1': 'var(--colour-theme-1)',
+				'colour-theme-2': 'var(--colour-theme-2)',
+				'colour-text': 'var(--colour-text)'
+			}
+		}
+	},
+	plugins: []
+};
