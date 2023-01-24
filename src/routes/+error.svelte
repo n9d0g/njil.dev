4fd22b2619448dcb4c479a383df6922415b56d19
n9d0g <nathanjlardizabal@gmail.com>
2023-01-23 20:48:18 -0500

    feat(pages): :sparkles: 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>n9 | 404</title>
+</svelte:head>
+
+<section class="flex flex-col container justify-center items-center">
+	<h1>404</h1>
+	<p>hey go back to my portfolio site >:(</p>
+</section>
