9e666121188d72a35fe36f28ace02e43b3c81cc4
n9d0g <nathanjlardizabal@gmail.com>
2023-08-14 23:16:52 -0400

    feat: :truck: basic routing implemented

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			minHeight: {
+				main: "calc(100vh - 80px)"
+			}
+		},
 	},
 	plugins: [],
 }
