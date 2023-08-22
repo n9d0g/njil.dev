eaf7962f035e5553f2f9903efe58cbb9ddb191b9
n9d0g <nathanjlardizabal@gmail.com>
2023-08-22 05:17:28 -0400

    feat: :sparkles: spotify API footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import partytown from '@astrojs/partytown'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), partytown()],
+	integrations: [tailwind(), partytown(), svelte()],
 })
