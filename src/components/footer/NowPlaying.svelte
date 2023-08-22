b645a10d73942765f806bdc7c586148c54288e88
n9d0g <nathanjlardizabal@gmail.com>
2023-08-22 11:26:46 -0400

    refactor: :truck: move `NowPlaying` into svelte astro island

---
+++ src/components/footer/NowPlaying.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data 
+  export let dataLink
+  export let artist
+  export let artistLink
+</script>
+
+<div class="flex flex-col">
+  <a href={dataLink} target="_blank" class="hover:underline text-normal">{data}</a>
+  <a href={artistLink} target="_blank" class="hover:underline text-xs italic">{artist}</a>
+</div>
