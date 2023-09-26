8bbc23df4130d1cdf723403cbc5a694002e08f4d
n9d0g <nathanjlardizabal@gmail.com>
2023-09-26 01:01:44 -0400

    feat: :sparkles: github contribution graph on homepage

---
+++ src/pages/api/contributions.json.ts
@@ -0,0 +1,49 @@
+import { parseHTML } from 'linkedom'
+
+export async function GET() {
+	const api =
+		'https://github.com/users/n9d0g/contributions?from=2023-01-01&to=2023-12-30'
+	const response = await fetch(api)
+	const data = await response.text()
+
+	const { document } = parseHTML(data)
+
+	const rows: any = document.querySelectorAll('tbody > tr')
+
+	// TODO: fix type
+	const contributions: any = []
+
+	for (const row of rows) {
+		const days = row.querySelectorAll('td:not(.ContributionCalendar-label)')
+		console.log(days)
+
+		// TODO: fix type
+		const currentRow: any = []
+
+		for (const day of days) {
+			const data = day.innerText.split(' ')
+
+			if (data.length > 1) {
+				const contribution = {
+					count: data[0] === 'No' ? 0 : +data[0],
+					name: data[3].replace(',', ''),
+					month: data[4],
+					day: +data[5].replace(',', ''),
+					year: data[6],
+					level: +day.dataset.level!,
+				}
+
+				currentRow.push(contribution)
+			} else {
+				currentRow.push(null)
+			}
+		}
+
+		contributions.push(currentRow)
+	}
+	return new Response(
+		JSON.stringify({
+			contributions: contributions,
+		})
+	)
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,13 @@ module.exports = {
 			minHeight: {
 				main: 'calc(100vh - 112px)',
 			},
+			colors: {
+				gh0: '#2c333b',
+				gh1: '#00442a',
+				gh2: '#006d35',
+				gh3: '#00a648',
+				gh4: '#00d35c',
+			},
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
