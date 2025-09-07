546555d4f0e2260dfe3096f292e8930fce097065
Nathan Lardizabal <nate@njil.dev>
2025-09-07 17:34:30 -0400

    refactor: :wrench: ts fixes

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,15 @@
-import { defineAction } from 'astro:actions'
+import { defineAction, ActionError } from 'astro:actions'
 import { z } from 'astro:schema'
+import { ContactSchema } from '@validation/form'
 
 export const server = {
 	contact: defineAction({
 		accept: 'form',
-		input: z.object({
-			name: z.string(),
-			email: z.string().email(),
-			message: z.string(),
-		}),
+		input: ContactSchema,
 		handler: async ({ name, email, message }) => {
-			console.log(name, email, message)
+			const recaptchaKey = import.meta.env.GOOGLE_RECAPTCHA_SECRET_KEY
+
+			console.log(name, email, message, recaptchaKey)
 			return `handled`
 		},
 	}),
--- src/validation/form.ts
+++ src/validation/form.ts
@@ -2,7 +2,7 @@ import z from 'zod'
 
 export const ContactSchema = z.object({
 	name: z.string().min(1, 'Name is required').max(100, 'Name is too long'),
-	email: z.string().email('Invalid email address'),
+	email: z.email('Invalid email address'),
 	message: z
 		.string()
 		.min(10, 'Message must be at least 10 characters')
