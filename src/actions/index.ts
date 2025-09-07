1464ed9957ba01b3dee4184ab1c17846ed94a06e
Nathan Lardizabal <nate@njil.dev>
2025-09-07 18:42:11 -0400

    perf: :chart_with_upwards_trend: vercel analytics; ts fixes

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,15 @@
 import { defineAction, ActionError } from 'astro:actions'
 import { z } from 'astro:schema'
-import { ContactSchema } from '@validation/form'
 
 export const server = {
 	contact: defineAction({
 		accept: 'form',
-		input: ContactSchema,
+		input: z.object({
+			name: z.string(),
+			email: z.string().email(),
+			message: z.string(),
+			recaptchaToken: z.string(),
+		}),
 		handler: async ({ name, email, message }) => {
 			const recaptchaKey = import.meta.env.GOOGLE_RECAPTCHA_SECRET_KEY
 
---
+++ src/types/layout.ts
@@ -0,0 +1,8 @@
+export interface BlogData {
+	title: string
+	author: string
+	published: Date
+	description: string
+	tags: string[]
+	draft: boolean
+}
