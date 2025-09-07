8fd9ea029681e5ef8d864841c22b31d35379c872
Nathan Lardizabal <nate@njil.dev>
2025-09-07 15:48:57 -0400

    feat: :sparkles: working form action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,16 @@
 import { defineAction } from 'astro:actions'
+import { z } from 'astro:schema'
 
 export const server = {
 	contact: defineAction({
-		handler: async (input) => {
+		accept: 'form',
+		input: z.object({
+			name: z.string(),
+			email: z.string().email(),
+			message: z.string(),
+		}),
+		handler: async ({ name, email, message }) => {
+			console.log(name, email, message)
 			return `handled`
 		},
 	}),
