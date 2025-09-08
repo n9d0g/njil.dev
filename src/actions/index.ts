2ff3d565b9aa3a884fd2059f7c0dcd4a755b2f03
Nathan Lardizabal <nate@njil.dev>
2025-09-08 15:08:46 -0400

    feat: :sparkles: contact form submission with recaptcha + nodemailer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 import { defineAction, ActionError } from 'astro:actions'
 import { z } from 'astro:schema'
+import nodemailer from 'nodemailer'
 
 export const server = {
 	contact: defineAction({
@@ -7,8 +8,64 @@ export const server = {
 		input: z.instanceof(FormData),
 		handler: async (data: FormData) => {
 			const recaptchaSecretKey = import.meta.env.GOOGLE_RECAPTCHA_SECRET_KEY
+			const emailUser = import.meta.env.EMAIL_USER
+			const emailPassword = import.meta.env.EMAIL_PASS
+			const name = (data.get('name') as string) ?? ''
+			const email = (data.get('email') as string) ?? ''
+			const message = (data.get('message') as string) ?? ''
 
-			return `handled`
+			try {
+				if (!emailUser || !emailPassword) {
+					throw new ActionError({
+						message: 'Missing email environment variables.',
+						code: 'INTERNAL_SERVER_ERROR',
+					})
+				}
+
+				// recaptcha
+				const token = data.get('token')
+				const response = await fetch(
+					`https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${token}`
+				)
+				const recaptchaData = await response.json()
+
+				if (!recaptchaData.success || recaptchaData.score < 0.7) {
+					throw new ActionError({
+						message: 'Recaptcha verification failed',
+						code: 'BAD_REQUEST',
+					})
+				}
+
+				// nodemailer
+				const transporter = nodemailer.createTransport({
+					service: 'gmail',
+					auth: { user: emailUser, pass: emailPassword },
+				})
+
+				const mailOptions = {
+					from: emailUser,
+					to: emailUser,
+					bcc: 'nathanjlardizabal@gmail.com',
+					subject: 'Contact Form Submission (njil.dev)',
+					html: `
+					<section>
+						<p>Name: ${name}</p>
+						<p>Email: ${email}</p>
+						<p>Message: ${message}</p>
+					</section>
+				`,
+				}
+
+				await transporter.sendMail(mailOptions)
+
+				console.log('Email sent successfully')
+				return {
+					message: 'Email sent successfully',
+				}
+			} catch (error) {
+				console.error(error)
+				throw error
+			}
 		},
 	}),
 }
