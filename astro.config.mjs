dcc98d50b933797229a052381e500d4c6197abce
Nathan Lardizabal <nate@njil.dev>
2025-09-08 21:18:48 -0400

    test: :test_tube: serverless vercel edge runtime test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
 	integrations: [tailwind(), partytown()],
