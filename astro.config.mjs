704b5b56f7606ab2c232589b6524e5a26c7cd3f4
Nathan Lardizabal <nate@njil.dev>
2025-09-08 21:14:36 -0400

    chore: :package: upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), partytown()],
 	output: 'server',
