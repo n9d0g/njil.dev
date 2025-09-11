5569b614b3fefb48149381eae763fc2160c3de95
Nathan Lardizabal <nate@njil.dev>
2025-09-11 19:01:51 -0400

    perf: :arrow_up: tailwind v4; typography wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
 import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	integrations: [tailwind(), partytown()],
+	integrations: [partytown()],
 	output: 'server',
 	adapter: vercel({ webAnalytics: true }),
+	site: 'https://www.njil.dev',
+	server: {
+		port: 3001,
+		host: true,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 })
