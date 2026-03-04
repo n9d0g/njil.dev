21537916dc8302dc987bc069e985cb23322e7ccb
Nathan Lardizabal <nate@njil.dev>
2026-03-04 15:54:49 -0500

    feat: :bug: icon package update; recaptcha copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config'
 import partytown from '@astrojs/partytown'
 import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
+import icon from 'astro-icon'
 
 import sanity from '@sanity/astro'
 
 export default defineConfig({
 	integrations: [
 		partytown(),
+		icon(),
 		sanity({
 			projectId: 'nbid6gbs',
 			dataset: 'production',
